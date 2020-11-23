a1443bc776d829d4ff0359260de05b0f16f938c7
Samuel-CHLam <chun.lam18@imperial.ac.uk>
2020-11-23 00:37:02 +0000

    Inefficient Plot of KS

--- 675sim.jl
+++ 675sim.jl
@@ -2,15 +2,35 @@ using Distributions, StatsBase, Random, Plots
 
 Random.seed!(1)
 
-dist = Exponential()
+dist = Uniform()
 
-x = [rand(dist, 100) for i in 1:15]
+function ecdf_samp(dist,itr,samp)
+    v = [Float32[] for i in 1:samp]
+    x = [rand(dist, itr) for i in 1:samp]
+    pl = plot(x -> cdf.(dist, x), -0.1, 10, title = "Empirical Exponential CDF", linewidth = 5, label="Original CDF", xlabel = "x", ylabel = "CDF", xrange = [-0.1,1.1])
+    for i in 1:itr
+        for j in 1:samp
+            append!(v[j], x[j][i])
+        end
+        norm_ecdf = ecdf(v[samp])
+        plot!(x -> norm_ecdf(x), -0.1, 10, legend=false)
+    end
+    display(pl)
+end
 
-v = [Float32[] for i in 1:15]
 
-for i in 1:100
-    pl = plot(x -> cdf.(dist, x), -0.1, 10, title = "Empirical Exponential CDF", linewidth = 5, label="Original CDF", xlabel = "x", ylabel = "CDF")
-    for j in 1:15
+
+
+## Output animation
+samp = 50
+itr = 150
+
+x = [rand(dist, itr) for i in 1:samp]
+v = [Float32[] for i in 1:samp]
+
+for i in 1:itr
+    pl = plot(x -> cdf.(dist, x), -0.1, 10, title = "Empirical Exponential CDF", linewidth = 5, label="Original CDF", xlabel = "x", ylabel = "CDF", xrange = [-0.1,1.1])
+    for j in 1:samp
         append!(v[j], x[j][i])
         norm_ecdf = ecdf(v[j])
         plot!(x -> norm_ecdf(x), -0.1, 10, legend=false)
